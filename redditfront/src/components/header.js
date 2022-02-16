fc7b388b20efa0f9c113b1a167640587034e70d3
WConanSantacruz <up190817@alumnos.upa.edu.mx>
2022-02-16 10:36:40 -0600

    Icons

---
+++ redditfront/src/components/header.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div classname="header">
+        <div classname="logo">
+            <img src="../../RedditISEI/assets/images/logo.png" classname="iconHeader" alt="logo">
+            <div classname="title">Chism</div>          
+        </div>
+        <div classname="buscador">
+            <input type="text" placeholder="Buscar" classname="searcher">
+            <div classname="iconSearch">
+                <i classname="fi fi-rr-search"></i>            
+            </div>
+        </div>
+        <div classname="signIn">
+            <div classname="loginText">Iniciar Sesión</div>
+        </div>
+        <div classname="user">
+            <div classnamename="">
+                <i classname="fi fi-rr-user"></i>            
+            </div>
+            <div>
+                <i classname="fi-rr-angle-down"></i>
+            </div>
+        </div>
+        
+    </div>
+  );
+}
+
+export default App;
